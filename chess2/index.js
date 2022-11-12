83e95f56d11c58c80a5dcb2485bba39b35da8bad
Abelz3w <78069279+Abelz3w@users.noreply.github.com>
2022-11-12 12:35:03 -0600

    Update index.js

--- chess2/index.js
+++ chess2/index.js
@@ -19,6 +19,7 @@ canvas.width = boardWidth;
 canvas.height = boardHeight;
 let fillRatio = .75;
 let selected = [-1, -1];
+let turns = ["W", "B"]
 let draggedCoords = [0, 0];
 let possibleMoves = [];
 let pieceImages = {
@@ -49,17 +50,18 @@ pieceImages.WK.src = "images/Pieces/WK.png";
 pieceImages.WP.src = "images/Pieces/WP.png";
 let board = [
     ["BR", "BN", "BB", "BQ", "BK", "BB", "BN", "BR"],
-    ["BP", "BP", "BP", "BP", "BP", "  ", "BP", "BP"],
+    ["  ", "BP", "BP", "BP", "BP", "  ", "BP", "BP"],
     ["  ", "  ", "  ", "  ", "  ", "  ", "  ", "  "],
     ["  ", "  ", "WP", "  ", "  ", "  ", "  ", "  "],
-    ["  ", "WP", "WR", "  ", "  ", "BP", "  ", "  "],
+    ["BP", "WP", "WR", "  ", "  ", "BP", "  ", "  "],
     ["  ", "  ", "WP", "  ", "  ", "  ", "  ", "  "],
     ["WP", "  ", "  ", "  ", "WP", "WP", "WP", "WP"],
-    ["WR", "WN", "  ", "WQ", "WK", "WB", "WN", "WR"],
+    ["WR", "WN", "WB", "WQ", "WK", "WB", "WN", "WR"],
 ];
 
 function draw() {
     const ctx = canvas.getContext("2d");
+    // Draw peices
     for (let h = 0; h < yDim; h++) {
 
         for (let w = 0; w < xDim; w++) {
@@ -72,21 +74,32 @@ function draw() {
         }
     }
 
+    if(selected != undefined && selected[0] != -1){
+        ctx.globalAlpha = .25;
+        ctx.fillStyle = "yellow"; 
+        ctx.fillRect((boardWidth / xDim) * selected[0], (boardHeight / yDim) * selected[1], boardWidth / xDim, boardHeight / yDim);
+        ctx.globalAlpha = 1;
+
+    }
+    
+    
+
     // draw possible moves
     if(possibleMoves != undefined && possibleMoves.length > 0){
         possibleMoves.forEach(pMove => {
             if(!pMove[2]){
-                console.log("ay")
                 ctx.fillStyle = "green"; 
                 ctx.fillRect((boardWidth / xDim) * pMove[0] + boardWidth / xDim * ((1 - fillRatio) / 2), (boardHeight / yDim) * pMove[1] + boardHeight / yDim * ((1 - fillRatio) / 2), boardWidth / xDim * fillRatio, boardHeight / yDim * fillRatio)
+    
+            }else{
+                ctx.fillStyle = "red"; 
+                ctx.fillRect((boardWidth / xDim) * pMove[0], (boardHeight / yDim) * pMove[1] , boardWidth / xDim, boardHeight / yDim)
     
             }
     
         })
     }
     
-
-    // Draw peices
     for (let h = 0; h < yDim; h++) {
 
         for (let w = 0; w < xDim; w++) {
@@ -97,12 +110,15 @@ function draw() {
                 ctx.fillRect((boardWidth / xDim) * w, (boardHeight / yDim) * h, boardWidth / xDim, boardHeight / yDim);
 
 
-            } else if (dragging && selected[0] == w && selected[1] == h) {
-                ctx.drawImage(pieceImages[board[h][w]], draggedCoords[0] - boardWidth / xDim * ((1 - fillRatio)) * 2, draggedCoords[1] - boardHeight / yDim * ((1 - fillRatio)) * 2, boardWidth / xDim * fillRatio, boardHeight / yDim * fillRatio)
-
-            }
+            } 
         }
     }
+    // Draw dragged
+    if(selected != undefined && selected[0] != -1 && dragging){
+
+        ctx.drawImage(pieceImages[board[selected[1]][selected[0]]], draggedCoords[0] - boardWidth / xDim * ((1 - fillRatio)) * 2, draggedCoords[1] - boardHeight / yDim * ((1 - fillRatio)) * 2, boardWidth / xDim * fillRatio, boardHeight / yDim * fillRatio)
+    }
+    
 
     
 }
@@ -119,16 +135,25 @@ function selectPiece(canvas, event) {
     let xSquare = Math.floor((x / boardWidth) * xDim);
     let ySquare = Math.floor((y / boardHeight) * yDim);
 
+    if(selected[0] != -1){
+        let checkPick = placeSelectedPeice(xSquare, ySquare)
+        if(!checkPick){
+            return 0;
+        }
+    }
     if (board[ySquare][xSquare][0] != " " && board[ySquare][xSquare][0] != "X") {
         dragging = true;
         selected = [xSquare, ySquare]
         draggedCoords[0] = x;
         draggedCoords[1] = y;
-
         if(board[ySquare][xSquare][1] == "R"){
-            possibleMoves = rookCalc(board, xSquare, ySquare, board[ySquare][xSquare][0])
+            possibleMoves = rookCalc(board, xSquare, ySquare, turn)
             
-        }else{
+        }else if(board[ySquare][xSquare][1] == "P"){
+            possibleMoves = pawnCalc(board, xSquare, ySquare, "up", turn)
+
+        }
+        else{
             possibleMoves = [];
         }
     }else{
@@ -167,5 +192,28 @@ function placeDraggedPeice(canvas, event) {
     let xSquare = Math.floor((x / boardWidth) * xDim);
     let ySquare = Math.floor((y / boardHeight) * yDim);
     dragging = false
+    
+    placeSelectedPeice(xSquare, ySquare)
+
+}
+function placeSelectedPeice(xSquare, ySquare){
+    let flag = true;
+    if(possibleMoves != undefined && possibleMoves.length > 0){
+        possibleMoves.every(pMove => {
+            if(pMove[0] == xSquare && pMove[1] == ySquare){
+                board[ySquare][xSquare] = board[selected[1]][selected[0]];
+                
+                turn = turns[(turns.indexOf(turn)+1)%turns.length]
+                board[selected[1]][selected[0]] = "  ";
+                selected =  [-1, -1];
+                possibleMoves = [];
+                flag = false
+                return false;
+            }
+            return true;
+        })
+
+    }  
+    return flag;
 }
 setInterval(draw, 10);
