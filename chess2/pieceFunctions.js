c2ea6f58876cfff1bce0b99f287905e4207c224c
Abelz3w <78069279+Abelz3w@users.noreply.github.com>
2022-11-12 12:35:27 -0600

    Update pieceFunctions.js

--- chess2/pieceFunctions.js
+++ chess2/pieceFunctions.js
@@ -2,6 +2,7 @@
 
 
 function rookCalc(board, positionX, positionY, color) {
+    if(color != board[positionY][positionX][0] ) return [];
     // X, Y, Takeable
     let possibleMoves = [];
     // up
@@ -32,7 +33,7 @@ function rookCalc(board, positionX, positionY, color) {
         possibleMoves.push([x, positionY, false]);
         x--;
     }
-    if (x >= 0 && board[x][positionY][0] != color && board[x][positionY][0] != "X") {
+    if (x >= 0 && board[positionY][x][0] != color && board[positionY][x][0] != "X") {
         possibleMoves.push([x, positionY, true]);
     }
 
@@ -42,9 +43,26 @@ function rookCalc(board, positionX, positionY, color) {
         possibleMoves.push([x, positionY, false]);
         x++;
     }
-    if (x < board[positionY].length && board[x][positionY][0] != color && board[x][positionY][0] != "X") {
+    if (x < board[positionY].length && board[positionY][x][0] != color && board[positionY][x][0] != "X") {
         possibleMoves.push([x, positionY, true]);
     }
 
     return possibleMoves;
+}
+function pawnCalc(board, positionX, positionY, direction, turn){
+    let possibleMoves = [];
+    
+    if(board[positionY][positionX][0] != turn) return [];
+    if(direction == "up"){
+        if(board[positionY-1][positionX] == "  "){
+            possibleMoves.push([positionX, positionY-1, false]);
+        }if(positionX > 0 && positionY >0 &&board[positionY-1][positionX-1][0] != turn && board[positionY-1][positionX-1][0] != " " ){
+            possibleMoves.push([positionX-1, positionY-1, true]);
+
+        }if(positionX < board[positionY].length && positionY >0 &&board[positionY-1][positionX+1][0] != turn && board[positionY-1][positionX+1][0] != " " ){
+            possibleMoves.push([positionX+1, positionY-1, true]);
+
+        }
+    }
+    return possibleMoves
 }
